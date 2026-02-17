8e1a951b8a67ebdf1778ebe22b188cbff9115523
benmak25 <102493266+benmak25@users.noreply.github.com>
2026-02-17 10:28:45 -0500

    Enhance Queries.sql with correlation coefficient calculation
    Added a query to calculate the correlation coefficient between GDP PPP and GDP Nominal.

--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,22 @@
 SELECT Year
 FROM Economy_of_US
-WHERE GDP_Growth > 0.05
+WHERE GDP_Growth > 0.05;
+
+SELECT
+  (
+    SUM((GDP_PPP - avg_x) * (GDP_Nominal - avg_y)) -- Numerator: Sum of products of deviations
+  ) / NULLIF(
+    SQRT(
+      SUM((GDP_PPP - avg_x) * (GDP_PPP- avg_x)) * SUM((GDP_Nominal - avg_y) * (GDP_Nominal - avg_y))
+    ),
+    0
+  ) AS correlation_coefficient
+FROM
+  (
+    SELECT
+      t.*,
+      AVG(GDP_PPP) OVER () AS avg_x,
+      AVG(GDP_Nominal) OVER () AS avg_y
+    FROM
+      Economy_of_US t
+  ) AS subquery_with_averages;
